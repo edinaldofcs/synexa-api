73ea88eb12a6e6292df5bf1eddd1a5c1d51880a3
edinaldofcs <edinaldofcs@gmail.com>
2026-02-17 18:25:28 -0300

    fix: register app controller and update env loading for dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,15 @@ import { AdminModule } from './admin/admin.module';
 import { TablesModule } from './tables/tables.module';
 import { ChatModule } from './chat/chat.module';
 import { PainelClientsModule } from './painel-clients/painel-clients.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.dev', '.env'],
+    }),
     CommonModule,
     ImportsModule,
     AdminModule,
@@ -18,5 +23,7 @@ import { PainelClientsModule } from './painel-clients/painel-clients.module';
     ChatModule,
     PainelClientsModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
